7014a630c2543f3a4017b400d071781d9f1f873d
Katharina Krassnitzer <k1krassnit@edu.aau.at>
2025-07-04 16:21:25 +0200

    Set up logic for buttons

--- app/src/main/java/at/terminplaner/LoginActivity.java
+++ app/src/main/java/at/terminplaner/LoginActivity.java
@@ -1,6 +1,12 @@
 package at.terminplaner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,34 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText emailLoginEditText = findViewById(R.id.editText_email_login);
+        EditText passwordLoginEditText = findViewById(R.id.editText_password_login);
+        Button loginButton = findViewById(R.id.button_login);
+        TextView toSignUp = findViewById(R.id.textView_to_sign_up);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailLoginEditText.getText().toString().trim();
+                String password = passwordLoginEditText.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    //TODO: Handle login logic here
+                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        toSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
