f3af180a75533d3734fb438b725419ff2808be41
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-07-16 10:13:52 +0200

    enhanced regrex for DATE pattern

--- app/src/main/java/at/terminplaner/DateTimeRecognizer.java
+++ app/src/main/java/at/terminplaner/DateTimeRecognizer.java
@@ -16,7 +16,25 @@ public class DateTimeRecognizer {
         Pattern pattern = null;
 
         if (type == Type.DATE) {
-            pattern = Pattern.compile("\\b\\d{1,2}[.\\-/]\\d{1,2}([.\\-/](\\d{2}|\\d{4}))?\\b(?!\\s?(Uhr|h|H|:|\\^))");
+            pattern = Pattern.compile(
+                    "\\b(?:" + // word limit for start
+                            "\\d{1,2}" + // 1 or 2 numbers are allowed
+                            "[.\\-/]" + // followed by ".", "-" or "/"
+                            "\\d{1,2}" +
+                            "(?:[.\\-/]\\d{2,4})?" +  // optional year with 2 or 4 numbers
+                            // for dates like 4.3, 13-01-2023, 29/02/11
+                            "|" +
+                            "\\d{4}" + // 4 numbers for year
+                            "[.\\-/]" +
+                            "\\d{1,2}" + // month
+                            "[.\\-/]" +
+                            "\\d{1,2}" + // day
+                            ")\\b" + // word limit for end
+                            "(?!" + // Negative Lookahead → the expression must not follow
+                            "\\s?" + // optional space
+                            "(Uhr|h|:|\\^))"
+                    // for dates like 2020-12-12, 2017/12/16
+            );
         } else if (type == Type.TIME) {
             pattern = Pattern.compile(
                     "\\b(?<!\\d[\\.\\-/])(?:[0-1]?\\d|2[0-3])(?:" +
