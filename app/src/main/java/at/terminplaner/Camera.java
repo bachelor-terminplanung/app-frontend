5441dde9b39d3755bb22a4613c5d3cb6cf47b91d
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-07-21 14:17:52 +0200

    added onCreate and onActivityResult

--- app/src/main/java/at/terminplaner/Camera.java
+++ app/src/main/java/at/terminplaner/Camera.java
@@ -1,4 +1,31 @@
 package at.terminplaner;
 
-public class Camera {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Camera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cloudocr);;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
