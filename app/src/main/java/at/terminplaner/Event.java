77578ca6604c628e50aef9700e947e1195eca763
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-07-25 18:59:09 +0200

    added variables and constructor for Event

--- app/src/main/java/at/terminplaner/Event.java
+++ app/src/main/java/at/terminplaner/Event.java
@@ -1,4 +1,21 @@
 package at.terminplaner;
 
 public class Event {
+    public String description;
+    public String date;
+    public String time;
+    public int duration;
+    public boolean isRepeating;
+    public String repeatType;
+    public String repeatUntil;
+
+    public Event(String description, String date, String time, int duration, boolean isRepeating, String repeatType, String repeatUntil) {
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.duration = duration;
+        this.isRepeating = isRepeating;
+        this.repeatType = repeatType;
+        this.repeatUntil = repeatUntil;
+    }
 }
